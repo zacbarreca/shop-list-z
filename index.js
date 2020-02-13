e6ece59b0f1ec56d1e307481e3ca29e0b3bca01a
Zac Barreca <zacbarreca@gmail.com>
2020-02-13 07:59:44 -0500

    create add shopping item functionality for index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-//comment
+'use strict';
+
+function handleClicks(){
+  console.log('Hello world');
+  let list = $('.shopping-list');
+  let form = $('#js-shopping-list-form');
+
+  form.submit(function(event){
+    console.log('Hello world');
+    event.preventDefault();
+    
+    let input = $('#shopping-list-entry');
+    let entry = input.val();
+
+    if (!entry){
+      input.focus();
+      return false;
+    }
+
+    input.val('');
+
+    let newElement = `
+    <li>
+      <span class="shopping-item">${entry}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>
+    `;
+
+    list.append($(newElement));
+  });
+}
+
+$(handleClicks);
